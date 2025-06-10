34309aaceceb6da75efeee2b8b57241f6d14a7f6
Vivek Kumar <vivek@Mac.lan>
2025-06-10 05:35:10 +0530

    added general user name

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="home">
-      <h1 className='content_title'>Hello, Vivek</h1> 
+      <h1 className='content_title'>Hello, User</h1> 
     </div>
   )
 }
